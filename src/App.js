353fb9542b4b29b5baa95a2bf830948087a6cfe1
Fuakim <cristianfallas12@live.com>
2022-10-04 23:05:02 -0600

    make it better

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
+import React from "react";
 //routing
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 //Components
-import Header from './components/Header';
-import Home from './components/Home';
-import Movie from './components/Movie';
-import NotFound from './components/NotFound';
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Movie from "./components/Movie";
+import NotFound from "./components/NotFound";
 
 // Styles
-import {GlobalStyle} from './GlobalStyle';
-
-
+import { GlobalStyle } from "./GlobalStyle";
 
 const App = () => (
   <Router>
-    <Header/>
+    <Header />
     <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='/:movieId' element={<Movie/>} />
-      <Route path='/*' element={<NotFound/>} />
+      <Route path="/" element={<Home />} />
+      <Route path="/:movieId" element={<Movie />} />
+      <Route path="/*" element={<NotFound />} />
     </Routes>
-    <GlobalStyle/>
+    <GlobalStyle />
   </Router>
 );
 
